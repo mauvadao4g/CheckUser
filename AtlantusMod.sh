b9030d5b6cb7c2fda7dada0dbc7704c6a91f6753
Mauvadao <108885597+mauvadao4g@users.noreply.github.com>
2023-08-15 07:25:06 -0300

    Update AtlantusMod.sh

--- AtlantusMod.sh
+++ AtlantusMod.sh
@@ -6,16 +6,16 @@ apt install npm -y
 cd /etc
 mkdir checkuser
 cd /etc/checkuser
-wget https://raw.githubusercontent.com/ScottBrabo/CheckUser/main/checkuser.js >/dev/null
+wget https://raw.githubusercontent.com/mauvadao4g/CheckUser/main/FILES/checkuser.js >/dev/null
 clear
-figlet 'SCOTTSSH' | lolcat
+figlet 'MauDaVpn' | lolcat
 echo '★ Digite a Senha da sua VPS:'
 tput civis
 read pwdroot
 sed -i "s/meupau/$pwdroot/" /etc/checkuser/checkuser.js > /dev/null 2>&1
-wget https://raw.githubusercontent.com/ScottBrabo/CheckUser/main/package-lock.json >/dev/null
-wget https://raw.githubusercontent.com/ScottBrabo/CheckUser/main/package.json >/dev/null
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+wget https://raw.githubusercontent.com/mauvadao4g/CheckUser/main/FILES/package-lock.json >/dev/null
+wget https://raw.githubusercontent.com/mauvadao4g/CheckUser/main/FILES/package.json >/dev/null
+curl -o- https://raw.githubusercontent.com/mauvadao4g/CheckUser/main/FILES/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -23,7 +23,7 @@ nvm install 16
 npm install pm2 -g
 npm install
 clear
-figlet 'SCOTTSSH' | lolcat
+figlet 'MauDaVpn' | lolcat
 echo 'Instalado com Sucesso!'
 echo
 echo '★Ativar CheckUser★'
@@ -32,6 +32,3 @@ echo
 echo '★Desativar CheckUser'
 echo 'cd /etc/checkuser && pm2 stop checkuser.js'
 }
-
-
-
